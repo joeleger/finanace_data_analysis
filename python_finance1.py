c07d5a72056e53b3cada05239d282926e8c8de29
joeleger <joeleger@users.noreply.github.com>
2020-06-01 15:26:43 -0400

    initial commit for data analysis video 1

--- python_finance1.py
+++ python_finance1.py
@@ -7,7 +7,7 @@ import pandas_datareader.data as web
 style.use('ggplot')
 
 start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2019, 12, 31)
+end = dt.datetime(2016, 12, 31)
 
 df = web.DataReader('TSLA', 'yahoo', start, end)
-
+print(df.head(6))
